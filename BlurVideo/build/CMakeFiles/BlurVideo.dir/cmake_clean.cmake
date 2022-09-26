368236eba6b422ba72a5a431a9aa954f91a50100
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-09-26 15:36:09 -0500

    refactor(BlurVideo): 🖼 Project in VSCode

---
+++ BlurVideo/build/CMakeFiles/BlurVideo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BlurVideo"
+  "BlurVideo.pdb"
+  "CMakeFiles/BlurVideo.dir/main.cpp.o"
+  "CMakeFiles/BlurVideo.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlurVideo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BlurVideo/build/CMakeFiles/BlurVideo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BlurVideo.
+# This may be replaced when dependencies are built.
